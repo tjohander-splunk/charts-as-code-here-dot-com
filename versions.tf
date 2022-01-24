edaaec5c944e6b100eab3b0bc3512100b67eeb57
Thomas Johander <tjohander@splunk.com>
2022-01-24 12:41:44 -0600

    Removed Terraform Cloud Org

--- versions.tf
+++ versions.tf
@@ -1,8 +1,5 @@
 terraform {
   required_version = ">= 0.13"
-  cloud {
-    organization = "example-org-bdf487"
-  }
   required_providers {
     signalfx = {
       source = "splunk-terraform/signalfx"
